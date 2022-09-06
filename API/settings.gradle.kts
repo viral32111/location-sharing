1c8607e8341a110a2d88ef2d1ca90f97a13536e7
viral32111 <19510403+viral32111@users.noreply.github.com>
2022-09-06 13:00:14 +0100

    Add new Ktor Kotlin project for API

---
+++ API/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "com.viral32111.locationsharingapi"
---
+++ API/src/main/kotlin/com/viral32111/Application.kt
@@ -0,0 +1,11 @@
+package com.viral32111
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.viral32111.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+    }.start(wait = true)
+}
